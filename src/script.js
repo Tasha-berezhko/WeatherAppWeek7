e4289f60a0084335542f758dcfe7062593d54564
Tasha-berezhko <124585701+Tasha-berezhko@users.noreply.github.com>
2023-02-26 17:13:34 +0200

    Added forecast API

--- src/script.js
+++ src/script.js
@@ -58,8 +58,16 @@ function displayWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayForecast() {
@@ -84,7 +92,7 @@ function displayForecast() {
 }
 
 function search(city) {
-  let apiKey = "e8dec81490f528b6b7847357fa83bb2a";
+  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeather);
 }
